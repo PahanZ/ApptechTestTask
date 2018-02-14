635e1d4cd2425fc0a13d095aad3a828d6405f18f
Pahan.Z <zagvzdin.pakhan@rambler.ru>
2018-02-14 20:59:45 +0300

    transition components and correct styles for progress bar

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 import { Route, withRouter, Switch } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import '../../styles.scss';
 
 import Header from './Header';
@@ -12,17 +13,22 @@ import Page3 from './Page3/Main';
 const App = props => (
   <Fragment>
     <Header currentPage={props.location.pathname} />
-    <Switch>
-      <Route exact path="/" component={Page1} />
-      <Route exact path="/Page2" component={Page2} />
-      <Route exact path="/Page3" component={Page3} />
-    </Switch>
+    <TransitionGroup>
+      <CSSTransition key={props.location.key} classNames="fade" timeout={300}>
+        <Switch location={props.location}>
+          <Route exact path="/" component={Page1} />
+          <Route exact path="/Page2" component={Page2} />
+          <Route exact path="/Page3" component={Page3} />
+        </Switch>
+      </CSSTransition>
+    </TransitionGroup>
   </Fragment>
 );
 
 App.propTypes = {
   location: PropTypes.shape({
     pathname: PropTypes.string,
+    key: PropTypes.string,
   }).isRequired,
 };
 
